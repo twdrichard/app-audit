a4d8638ca2343e1709b615c725e6bc45ce12ab83
TWDRichard <richard@towerwebdesign.co.uk>
2026-02-02 14:28:11 +0000

    Basic local server info added

--- app/Commands/ServerInf.php
+++ app/Commands/ServerInf.php
@@ -26,21 +26,76 @@ class ServerInf extends Command
      */
     public function handle()
     {
-        echo "Hello, World!" . PHP_EOL;
-        $this->info("serverInf springs to life!");
-        /*
-    render(<<<'HTML'
-        <div>
-            <div class="px-1 bg-green-600 py-10 w-full">
-                <span> Mondev </span>
-            </div>
-            <em class="ml-1 py-10 text-red-500 font-bold">
-                The Developer's Newsletter 0_1
-            </em>
-        </div>
-    HTML);
-    */
+        $this->info("Hello, " . $this->findUsername() .  "!");
+        $basic_server_info = [
+            'hostname'      => "hostname",
+            'PHP version'   => "php --version",
+            //'OS version'    => "uname -svrm",
+            'is Linux'      => "if \"true\" == ',('; then echo Linux; fi;true \) else echo Windows",
+        ];
+        foreach ($basic_server_info as $info_name => $command) {
+            $value = $this->executeCommand($command);
+            $this->info($info_name . ": " . $value);
         }
+        $this->info('Linux version: ' . $this->findLinuxVersion());
+    }
+
+	protected function findUsername() : string {
+		$command = "whoami";
+		$output = [];
+		exec($command, $output, $result);
+		if ($result == 0) {
+			if ($output && count($output)) {
+				$name = reset($output);
+				if ($name) {
+					$this->username = $name;
+					return ucfirst($name);
+				}
+			}
+		}
+		return "stranger";
+	}
+
+    protected function findLinuxVersion() : string {
+        $command = "cat /etc/*release";
+		$output = [];
+        $version_name = "unknown";
+		exec($command, $output, $result);
+		if ($result == 0) {
+            $version_name = "";
+            $os_name = "";
+            $os_version = "";
+            foreach ($output as $line) {
+                if ($line) {
+                    $ar = explode("=", $line);
+                    if ($ar && count($ar) > 1) {
+                        $item_name = $ar[0];
+                        $value = str_replace('"', '', $ar[1]);
+                        if ($item_name == "NAME") {
+                            $os_name = $value;
+                        }
+                        if ($item_name == "VERSION") {
+                            $os_version = $value;
+                        }
+                    }
+                }
+            }
+            $version_name = $os_name . ' ' . $os_version;
+        }
+        return $version_name;
+    }
+
+    protected function executeCommand(string $command) : string {
+		$output = [];
+		exec($command, $output, $result);
+		if ($result == 0) {
+			if ($output && count($output)) {
+				return reset($output);
+			}
+            return implode("\n", $output);
+		}
+		return "none.";
+    }
 
     /**
      * Define the command's schedule.
